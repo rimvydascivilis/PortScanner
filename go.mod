c40d85ae81e16aa955e0d71acc60e795e42f1d7e
Rimvydas <tryheadas@gmail.com>
2022-05-20 19:46:39 +0300

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rimvydascivilis/PortScanner
+
+go 1.18
--- main.go
+++ main.go
@@ -1,38 +1,77 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"os"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
 
+var portRange = flag.String("p", "0:65535", "Port range, syntax: -p <start>:<end>")
+
 func main() {
-	ip := "127.0.0.1"        // CHANGE THIS (IP)
-	port := [2]int{0, 65535} // CHANGE THIS (port range) default (0-65535)
-
-	fmt.Printf("Scanning host %v, ports %v-%v\n", ip, port[0], port[1])
-
+	flag.Parse()
 	var wg sync.WaitGroup
 	startedScanAt := time.Now()
 	openPortsCount := 0
+	host, portRangeStart, portRangeEnd := getFlags()
 
-	for i := port[0]; i <= port[1]; i++ {
-		wg.Add(1)
-		go scanPort(ip, i, &openPortsCount, &wg)
+	wg.Add(portRangeEnd - portRangeStart + 1)
+
+	fmt.Printf("Scanning host %v, ports %v-%v\n", host, portRangeStart, portRangeEnd)
+
+	for i := portRangeStart; i <= portRangeEnd; i++ {
+		go tcpScan(host, i, &openPortsCount, &wg)
 	}
 
 	wg.Wait()
-	fmt.Println("scanning completed in", time.Since(startedScanAt))
-	fmt.Println("open ports:", openPortsCount)
+	fmt.Println("Open ports:", openPortsCount)
+	fmt.Println("Scanning completed in", time.Since(startedScanAt))
 }
 
-func scanPort(ip string, port int, openPortsCount *int, wg *sync.WaitGroup) {
-	_, err := net.Dial("tcp", fmt.Sprintf("%v:%v", ip, port))
+func tcpScan(host string, port int, openPortsCount *int, wg *sync.WaitGroup) {
+	_, err := net.Dial("tcp", fmt.Sprintf("%v:%v", host, port))
 
 	if err == nil {
-		fmt.Printf("%v port is open\n", port)
+		fmt.Printf("[+] %v port is open\n", port)
 		*openPortsCount++
 	}
 	wg.Done()
+}
+
+func getFlags() (string, int, int) {
+	host := flag.CommandLine.Arg(0)
+	portRangeSlice := strings.Split(*portRange, ":")
+	portRangeStart, err := strconv.Atoi(portRangeSlice[0])
+	if err != nil {
+		fmt.Println("Invalid port range")
+		os.Exit(1)
+	}
+	portRangeEnd, err := strconv.Atoi(portRangeSlice[1])
+	if err != nil {
+		fmt.Println("Invalid port range")
+		os.Exit(1)
+	}
+	verifyFlags(host, portRangeSlice, portRangeStart, portRangeEnd)
+
+	return host, portRangeStart, portRangeEnd
+}
+
+func verifyFlags(host string, portRangeSlice []string, portRangeStart int, portRangeEnd int) {
+	if len(portRangeSlice) != 2 {
+		fmt.Println("Invalid port range")
+		os.Exit(1)
+	}
+	if host == "" {
+		fmt.Println("You must Specify a host")
+		os.Exit(1)
+	}
+	if portRangeEnd < portRangeStart {
+		fmt.Println("Invalid port range")
+		os.Exit(1)
+	}
 }
