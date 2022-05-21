2433633680f83bcf54a33869007effdbbe7dde9b
rimvydascivilis <tryheadas@gmail.com>
2022-05-21 08:28:44 +0300

    nmap implementation

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # Port scanner
 
 ## Usage
-### 1. From the command line in the PortScanner directory, run the go build command to compile the code into an executable.
+### 1. From the command line in the PortScanner directory, run the go build command to compile the code into an executable
   
   ```go build```
 
 ### 2. Run the executable
-
-  ```./PortScanner -p <port range from>:<port range to> <host>```
+  - ```./PortScanner --help``` to get help
   
- #### Examples:
-  - ```./PortScanner -p 100:1500 127.0.0.1```
-  - ```./PortScanner localhost``` (default port range 0-65535)
+#### Examples:
+  - ```./PortScanner -p 100:1500 -h <host>```
+  - ```./PortScanner -n ``` (default port range 0-65535, default host localhost)
--- main.go
+++ main.go
@@ -5,48 +5,83 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
 )
 
+var host = flag.String("h", "localhost", "Host to scan")
 var portRange = flag.String("p", "0:65535", "Port range, syntax: -p <start>:<end>")
+var nmapEnabled = flag.Bool("n", false, "Enable nmap scanning, you need to have nmap installed")
 
 func main() {
-	flag.Parse()
-	var wg sync.WaitGroup
-	startedScanAt := time.Now()
-	openPortsCount := 0
+	var openPorts []int
 	host, portRangeStart, portRangeEnd := getFlags()
 
+	findOpenTcpPorts(host, portRangeStart, portRangeEnd, &openPorts)
+
+	if *nmapEnabled {
+		if len(openPorts) == 0 {
+			fmt.Println("Nmap scan not started, no open ports found")
+			os.Exit(0)
+		}
+		startNmapScan(host, &openPorts)
+	}
+	fmt.Println("NEW")
+}
+
+func findOpenTcpPorts(host string, portRangeStart int, portRangeEnd int, openPorts *[]int) {
+	startedScanAt := time.Now()
+	var wg sync.WaitGroup
 	wg.Add(portRangeEnd - portRangeStart + 1)
 
 	fmt.Printf("Scanning host %v, ports %v-%v\n", host, portRangeStart, portRangeEnd)
 
 	for i := portRangeStart; i <= portRangeEnd; i++ {
-		go tcpScan(host, i, &openPortsCount, &wg)
+		go tcpScan(host, i, openPorts, &wg)
 	}
 
 	wg.Wait()
-	fmt.Println("Open ports:", openPortsCount)
+	fmt.Println("Open ports:", len(*openPorts))
 	fmt.Println("Scanning completed in", time.Since(startedScanAt))
 }
 
-func tcpScan(host string, port int, openPortsCount *int, wg *sync.WaitGroup) {
+func tcpScan(host string, port int, openPorts *[]int, wg *sync.WaitGroup) {
 	_, err := net.Dial("tcp", fmt.Sprintf("%v:%v", host, port))
 
 	if err == nil {
 		fmt.Printf("[+] %v port is open\n", port)
-		*openPortsCount++
+		*openPorts = append(*openPorts, port)
 	}
 	wg.Done()
 }
 
+func startNmapScan(host string, openPorts *[]int) {
+	var nmapArgs []string
+
+	nmapArgs = append(nmapArgs, "-p"+arrayToString(openPorts, ","))
+	nmapArgs = append(nmapArgs, "-A")
+	nmapArgs = append(nmapArgs, host)
+
+	fmt.Printf("Starting nmap scan 'nmap %v'\n", strings.Join(nmapArgs, " "))
+
+	cmd := exec.Command("nmap", nmapArgs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error running nmap:", err)
+	}
+}
+
 func getFlags() (string, int, int) {
-	host := flag.CommandLine.Arg(0)
+	flag.Parse()
 	portRangeSlice := strings.Split(*portRange, ":")
 	portRangeStart, err := strconv.Atoi(portRangeSlice[0])
+	host := *host
+
 	if err != nil {
 		fmt.Println("Invalid port range")
 		os.Exit(1)
@@ -56,22 +91,22 @@ func getFlags() (string, int, int) {
 		fmt.Println("Invalid port range")
 		os.Exit(1)
 	}
-	verifyFlags(host, portRangeSlice, portRangeStart, portRangeEnd)
 
+	verifyFlags(portRangeSlice, portRangeStart, portRangeEnd)
 	return host, portRangeStart, portRangeEnd
 }
 
-func verifyFlags(host string, portRangeSlice []string, portRangeStart int, portRangeEnd int) {
+func verifyFlags(portRangeSlice []string, portRangeStart int, portRangeEnd int) {
 	if len(portRangeSlice) != 2 {
 		fmt.Println("Invalid port range")
-		os.Exit(1)
-	}
-	if host == "" {
-		fmt.Println("You must Specify a host")
 		os.Exit(1)
 	}
 	if portRangeEnd < portRangeStart {
 		fmt.Println("Invalid port range")
 		os.Exit(1)
 	}
+}
+
+func arrayToString(a *[]int, delim string) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(*a), " ", delim, -1), "[]")
 }
